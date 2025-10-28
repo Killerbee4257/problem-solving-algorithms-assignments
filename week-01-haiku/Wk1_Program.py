a81985f37e7450e5a3bba485e6af1590b50173b1
Killerbee4257 <njw22001@gmail.com>
2025-10-28 23:58:36 +0900

    Add files via upload

---
+++ week-01-haiku/Wk1_Program.py
@@ -0,0 +1,11 @@
+'''
+Name: Nigel Wilkerson
+Date: 14 October 2025
+Week 1 Assignment 2: Programming Assignment
+'''
+
+print('"The Light of the Moon" by Matsuo Bashō')
+print("---------------------------------")
+print("An old silent pond")
+print("A frog jumps into the pond-")
+print("Splash! Silence again.")
